19bde84db11c677129e39584ea9a7e0a34999c0c
Natalia Maximo <nmaximo@microsoft.com>
2023-07-24 14:31:50 -0400

    moved validation to janus-common crate

--- janus-cli/src/lib.rs
+++ janus-cli/src/lib.rs
@@ -18,12 +18,9 @@ pub mod cli;
 pub mod logging;
 
 use janus::{Gateway, SensorGateway, SensorReading};
-use janus_common::ConfigFile;
-use jsonschema::JSONSchema;
-use std::{fs::File, sync::Arc};
+use janus_common::parse_configs;
+use std::sync::Arc;
 use tracing::debug;
-
-const GATEWAY_SCHEMA: &str = include_str!("../../schemas/gateway.schema.json");
 
 pub async fn run(cli: cli::Cli) {
     logging::setup_logging(&cli);
@@ -38,48 +35,6 @@ pub async fn run(cli: cli::Cli) {
     if let Err(e) = result {
         println!("{}", e);
     }
-}
-
-fn validate_config(config_value: &serde_json::Value) -> Result<(), String> {
-    let schema_value: serde_json::Value =
-        serde_json::from_str(GATEWAY_SCHEMA).map_err(|e| format!("Error parsing schema: {}", e))?;
-
-    let schema =
-        JSONSchema::compile(&schema_value).map_err(|e| format!("Error compiling schema: {}", e))?;
-
-    schema.validate(config_value).map_err(|e| {
-        let errors = e
-            .into_iter()
-            .map(|e| {
-                if e.instance_path.to_string() != "" {
-                    format!("\tError validating {}: {}", e.instance_path, e)
-                } else {
-                    format!("\tError validating schema: {}", e)
-                }
-            })
-            .collect::<Vec<String>>()
-            .join("\n");
-
-        format!("Validation failed:\n{}", errors)
-    })
-}
-
-fn parse_configs(file_name: &str) -> Result<ConfigFile, String> {
-    debug!("Validating file: {}", file_name);
-
-    let file_handler = File::open(file_name).map_err(|e| format!("Error opening file: {}", e))?;
-
-    let config: serde_json::Value =
-        serde_yaml::from_reader(file_handler).map_err(|e| format!("Error parsing YAML: {}", e))?;
-
-    validate_config(&config)?;
-
-    let config_file: ConfigFile = serde_json::from_value(config)
-        .map_err(|e| format!("Error parsing config into target: {}", e))?;
-
-    debug!("Deserialized config file: {:?}", &config_file);
-
-    Ok(config_file)
 }
 
 fn run_validate(args: cli::ValidateArgs) -> Result<(), String> {
---
+++ janus-common/src/validation.rs
@@ -0,0 +1,48 @@
+use crate::config::ConfigFile;
+
+use std::fs::File;
+use tracing::debug;
+
+pub const GATEWAY_SCHEMA: &str = include_str!("../../schemas/gateway.schema.json");
+
+fn validate_config(config_value: &serde_json::Value) -> Result<(), String> {
+    let schema_value: serde_json::Value =
+        serde_json::from_str(GATEWAY_SCHEMA).map_err(|e| format!("Error parsing schema: {}", e))?;
+
+    let schema = jsonschema::JSONSchema::compile(&schema_value)
+        .map_err(|e| format!("Error compiling schema: {}", e))?;
+
+    schema.validate(config_value).map_err(|e| {
+        let errors = e
+            .into_iter()
+            .map(|e| {
+                if e.instance_path.to_string() != "" {
+                    format!("\tError validating {}: {}", e.instance_path, e)
+                } else {
+                    format!("\tError validating schema: {}", e)
+                }
+            })
+            .collect::<Vec<String>>()
+            .join("\n");
+
+        format!("Validation failed:\n{}", errors)
+    })
+}
+
+pub fn parse_configs(file_name: &str) -> Result<ConfigFile, String> {
+    debug!("Validating file: {}", file_name);
+
+    let file_handler = File::open(file_name).map_err(|e| format!("Error opening file: {}", e))?;
+
+    let config: serde_json::Value =
+        serde_yaml::from_reader(file_handler).map_err(|e| format!("Error parsing YAML: {}", e))?;
+
+    validate_config(&config)?;
+
+    let config_file: ConfigFile = serde_json::from_value(config)
+        .map_err(|e| format!("Error parsing config into target: {}", e))?;
+
+    debug!("Deserialized config file: {:?}", &config_file);
+
+    Ok(config_file)
+}
