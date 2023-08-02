81d51b4267cd1f6ed5cb28bee9e3382cf86187ae
Natalia Maximo <nmaximo@microsoft.com>
2023-08-02 18:53:13 -0400

    Add tests to pixy-core and pixy-server

--- pixy-core/src/handlers/webhook.rs
+++ pixy-core/src/handlers/webhook.rs
@@ -20,6 +20,34 @@ pub struct WebhookHandler {
 }
 
 impl WebhookHandler {
+    /// Creates a new WebhookHandler given a target configuration and a reqwest client.
+    ///
+    /// ## Arguments
+    ///
+    /// * `target_config` - The target configuration.
+    /// * `client` - The reqwest client to use for requests.
+    ///
+    /// ## Examples
+    /// ```
+    /// use pixy_core::config::{Target, TargetProperties::Webhook, WebhookTargetProperties};
+    /// use pixy_core::handlers::WebhookHandler;
+    ///
+    /// let target = Target {
+    ///    name: "test".to_string(),
+    ///    enabled: true,
+    ///    properties: Webhook(WebhookTargetProperties {
+    ///       url: "https://example.com".to_string(),
+    ///       retries: 3,
+    ///       timeout: 5,
+    ///       auth: None,
+    ///   }),
+    /// };
+    ///
+    /// let client = reqwest::Client::new();
+    ///
+    /// let handler = WebhookHandler::new(target, client);
+    /// ```
+    ///
     #[instrument]
     pub fn new(target_config: Target, client: reqwest::Client) -> Self {
         let Webhook(properties) = target_config.properties else {
@@ -44,6 +72,31 @@ impl WebhookHandler {
 }
 
 impl From<Target> for WebhookHandler {
+    /// Creates a new WebhookHandler given a target configuration.
+    ///
+    /// ## Arguments
+    ///
+    /// * `target_config` - The target configuration.
+    ///
+    /// ## Examples
+    /// ```
+    /// use pixy_core::config::{Target, TargetProperties::Webhook, WebhookTargetProperties};
+    /// use pixy_core::handlers::WebhookHandler;
+    ///
+    /// let target = Target {
+    ///    name: "test".to_string(),
+    ///    enabled: true,
+    ///    properties: Webhook(WebhookTargetProperties {
+    ///       url: "https://example.com".to_string(),
+    ///       retries: 3,
+    ///       timeout: 5,
+    ///       auth: None,
+    ///   }),
+    /// };
+    ///
+    /// let handler = WebhookHandler::from(target);
+    /// ```
+    ///
     fn from(target_config: Target) -> Self {
         let client = clients::get_default_webhook_client();
 
@@ -119,5 +172,218 @@ impl SensorHandler for WebhookHandler {
 
     fn is_enabled(&self) -> bool {
         self.enabled
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use httpmock::{Method::POST, MockServer};
+    use minijinja::context;
+
+    const TEST_MESSAGE: &str = include_str!("../../../example-configs/test-sensor.json");
+
+    fn default_properties() -> WebhookTargetProperties {
+        WebhookTargetProperties {
+            url: "http://localhost:8080".to_string(),
+            retries: 3,
+            timeout: 10,
+            auth: None,
+        }
+    }
+
+    #[test]
+    fn test_webhook_handler_from_target() {
+        let target = Target {
+            name: "test".to_string(),
+            enabled: true,
+            properties: Webhook(default_properties()),
+        };
+
+        let handler = WebhookHandler::from(target);
+
+        assert_eq!(handler.name, "test");
+        assert_eq!(handler.config.url, "http://localhost:8080");
+        assert_eq!(handler.config.retries, 3);
+        assert_eq!(handler.config.timeout, 10);
+
+        assert!(handler.enabled);
+        assert!(handler.config.auth.is_none());
+    }
+
+    #[tokio::test]
+    async fn test_webhook_on_simple_target() {
+        let server = MockServer::start_async().await;
+
+        let message: SensorMessage = serde_json::from_str(TEST_MESSAGE).unwrap();
+
+        let body_contents = serde_json::to_string(&message).unwrap();
+
+        let mock: httpmock::Mock<'_> = server
+            .mock_async(|when, then| {
+                when.method(POST)
+                    .path("/")
+                    .header("Content-Type", "application/json")
+                    .body(&body_contents);
+                then.status(200);
+            })
+            .await;
+
+        server
+            .mock_async(|when, then| {
+                when.any_request();
+                then.status(400);
+            })
+            .await;
+
+        let mut properties = default_properties();
+
+        properties.url = server.url("/");
+
+        let target = Target {
+            name: "test".to_string(),
+            enabled: true,
+            properties: Webhook(properties),
+        };
+
+        let handler = WebhookHandler::from(target);
+
+        let result = handler.handle_reading(&message, &context!()).await;
+
+        assert!(result.is_ok());
+        mock.assert_async().await;
+    }
+
+    #[tokio::test]
+    async fn test_webhook_with_basic_auth() {
+        let server = MockServer::start_async().await;
+
+        let message: SensorMessage = serde_json::from_str(TEST_MESSAGE).unwrap();
+
+        let body_contents = serde_json::to_string(&message).unwrap();
+
+        let mock: httpmock::Mock<'_> = server
+            .mock_async(|when, then| {
+                when.method(POST)
+                    .path("/")
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", "Basic dXNlcm5hbWU6cGFzc3dvcmQ=")
+                    .body(&body_contents);
+                then.status(200);
+            })
+            .await;
+
+        server
+            .mock_async(|when, then| {
+                when.any_request();
+                then.status(400);
+            })
+            .await;
+
+        let mut properties = default_properties();
+
+        properties.url = server.url("/");
+        properties.auth = Some(WebhookAuth::Basic {
+            username: "username".to_string(),
+            password: "password".to_string(),
+        });
+
+        let target = Target {
+            name: "test".to_string(),
+            enabled: true,
+            properties: Webhook(properties),
+        };
+
+        let handler = WebhookHandler::from(target);
+
+        let result = handler.handle_reading(&message, &context!()).await;
+
+        assert!(result.is_ok());
+        mock.assert_async().await;
+    }
+
+    #[tokio::test]
+    async fn test_webhook_with_bearer_auth() {
+        let server = MockServer::start_async().await;
+
+        let message: SensorMessage = serde_json::from_str(TEST_MESSAGE).unwrap();
+
+        let body_contents = serde_json::to_string(&message).unwrap();
+
+        let mock: httpmock::Mock<'_> = server
+            .mock_async(|when, then| {
+                when.method(POST)
+                    .path("/")
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", "Bearer 123456789")
+                    .body(&body_contents);
+                then.status(200);
+            })
+            .await;
+
+        server
+            .mock_async(|when, then| {
+                when.any_request();
+                then.status(400);
+            })
+            .await;
+
+        let mut properties = default_properties();
+
+        properties.url = server.url("/");
+        properties.auth = Some(WebhookAuth::Bearer {
+            token: "123456789".to_string(),
+        });
+
+        let target = Target {
+            name: "test".to_string(),
+            enabled: true,
+            properties: Webhook(properties),
+        };
+
+        let handler = WebhookHandler::from(target);
+
+        let result = handler.handle_reading(&message, &context!()).await;
+
+        assert!(result.is_ok());
+        mock.assert_async().await;
+    }
+
+    #[tokio::test]
+    async fn test_webhook_client_retries() {
+        let server = MockServer::start_async().await;
+
+        let message: SensorMessage = serde_json::from_str(TEST_MESSAGE).unwrap();
+
+        let body_contents = serde_json::to_string(&message).unwrap();
+
+        let mock: httpmock::Mock<'_> = server
+            .mock_async(|when, then| {
+                when.method(POST)
+                    .path("/")
+                    .header("Content-Type", "application/json")
+                    .body(&body_contents);
+                then.status(500);
+            })
+            .await;
+
+        let mut properties = default_properties();
+
+        properties.url = server.url("/");
+        properties.retries = 1;
+        properties.timeout = 1;
+
+        let target = Target {
+            name: "test".to_string(),
+            enabled: true,
+            properties: Webhook(properties),
+        };
+
+        let handler = WebhookHandler::from(target);
+
+        let result = handler.handle_reading(&message, &context!()).await;
+
+        assert!(result.is_err());
+        mock.assert_hits_async(2).await;
     }
 }
--- pixy-server/src/lib.rs
+++ pixy-server/src/lib.rs
@@ -13,21 +13,24 @@ use crate::config::ServerConfiguration;
 use pixy_core::validation::parse_configs;
 use pixy_core::{Gateway, SensorGateway, SensorMessage};
 
-pub async fn run_server_with(server_configs: ServerConfiguration) {
-    let pixy_configs = parse_configs(&server_configs.config_file).unwrap();
-
-    let gateway: Arc<dyn Gateway> = Arc::new(SensorGateway::from(pixy_configs));
-
+fn create_app(gateway: Arc<dyn Gateway>, server_configs: &ServerConfiguration) -> axum::Router {
     let app = axum::Router::new()
         .route("/data", post(handler))
         .route("/healthz", get(|| async { StatusCode::OK }))
         .with_state(gateway);
 
-    let app = if server_configs.enable_echo {
+    if server_configs.enable_echo {
         app.route("/echo", post(echo))
     } else {
         app
-    };
+    }
+}
+
+pub async fn run_server_with_gateway(
+    gateway: Arc<dyn Gateway>,
+    server_configs: ServerConfiguration,
+) {
+    let app = create_app(gateway, &server_configs);
 
     let bind_address = format!("0.0.0.0:{}", server_configs.port);
 
@@ -54,6 +57,14 @@ pub async fn run_server_with(server_configs: ServerConfiguration) {
         .unwrap();
 }
 
+pub async fn run_server_with(server_configs: ServerConfiguration) {
+    let pixy_configs = parse_configs(&server_configs.config_file).unwrap();
+
+    let gateway: Arc<dyn Gateway> = Arc::new(SensorGateway::from(pixy_configs));
+
+    run_server_with_gateway(gateway, server_configs).await;
+}
+
 #[instrument]
 async fn handler(
     State(gateway): State<Arc<dyn Gateway>>,
@@ -72,4 +83,149 @@ async fn handler(
 async fn echo(data: String) -> String {
     info!("Received data: {:?}", &data);
     data
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use async_trait::async_trait;
+    use axum::http::{self, Request};
+    use tower::ServiceExt;
+
+    #[derive(Debug)]
+    struct MockGateway {}
+
+    #[async_trait]
+    impl Gateway for MockGateway {
+        async fn handle_reading(&self, _reading: SensorMessage) {}
+    }
+
+    fn default_config() -> ServerConfiguration {
+        ServerConfiguration {
+            config_file: String::new(),
+            port: 9147,
+            log_level: String::from("info"),
+            enable_echo: false,
+        }
+    }
+
+    #[tokio::test]
+    async fn test_health_endpoint() {
+        let gateway: Arc<dyn Gateway> = Arc::new(MockGateway {});
+
+        let app = create_app(gateway, &default_config());
+
+        let res = app
+            .oneshot(Request::get("/healthz").body("".into()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(res.status(), http::StatusCode::OK);
+    }
+
+    #[tokio::test]
+    async fn test_echo_enabled() {
+        let mut configs = default_config();
+
+        configs.enable_echo = true;
+
+        let gateway: Arc<dyn Gateway> = Arc::new(MockGateway {});
+
+        let app = create_app(gateway, &configs);
+
+        let res = app
+            .oneshot(Request::post("/echo").body("hello".into()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(res.status(), http::StatusCode::OK);
+
+        let body = hyper::body::to_bytes(res.into_body()).await.unwrap();
+
+        assert_eq!(&body[..], b"hello");
+    }
+
+    #[tokio::test]
+    async fn test_echo_disable() {
+        let gateway: Arc<dyn Gateway> = Arc::new(MockGateway {});
+
+        let app = create_app(gateway, &default_config());
+
+        let res = app
+            .oneshot(Request::post("/echo").body("hello".into()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(res.status(), http::StatusCode::NOT_FOUND);
+    }
+
+    #[tokio::test]
+    async fn test_example_sensor_works() {
+        let gateway: Arc<dyn Gateway> = Arc::new(MockGateway {});
+
+        let app = create_app(gateway, &default_config());
+
+        let example_sensor: SensorMessage =
+            serde_json::from_str(include_str!("../../example-configs/test-sensor.json")).unwrap();
+
+        let res = app
+            .oneshot(
+                Request::post("/data")
+                    .header("Content-Type", "application/json")
+                    .body(serde_json::to_string(&example_sensor).unwrap().into())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(res.status(), http::StatusCode::ACCEPTED);
+    }
+
+    #[tokio::test]
+    async fn test_fails_if_wrong_content_type() {
+        let gateway: Arc<dyn Gateway> = Arc::new(MockGateway {});
+
+        let app = create_app(gateway, &default_config());
+
+        let example_sensor: SensorMessage =
+            serde_json::from_str(include_str!("../../example-configs/test-sensor.json")).unwrap();
+
+        let res = app
+            .oneshot(
+                Request::post("/data")
+                    .body(serde_json::to_string(&example_sensor).unwrap().into())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(res.status(), http::StatusCode::UNSUPPORTED_MEDIA_TYPE);
+    }
+
+    #[tokio::test]
+    async fn test_malformed_sensor_fails() {
+        let gateway: Arc<dyn Gateway> = Arc::new(MockGateway {});
+
+        let app = create_app(gateway, &default_config());
+
+        let example_sensor: SensorMessage =
+            serde_json::from_str(include_str!("../../example-configs/test-sensor.json")).unwrap();
+
+        let res = app
+            .oneshot(
+                Request::post("/data")
+                    .header("Content-Type", "application/json")
+                    .body(
+                        serde_json::to_string(&example_sensor)
+                            .unwrap()
+                            .replace("temperature", "hot")
+                            .into(),
+                    )
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(res.status(), http::StatusCode::UNPROCESSABLE_ENTITY);
+    }
 }
